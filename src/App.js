c6e5acfde1afe93ed5b8e1319e9081ea1fd88210
Trey <tajohns22@gmail.com>
2019-10-10 11:41:30 -0500

    Upade app with shop page, collection preview and item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from "./pages/homepage.component";
 import {Route, Switch} from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () =>(
-  <div>
-    <h1>HATS</h1>
-  </div>
-)
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route  path="/shop/hats" component={HatsPage} />
+        <Route  path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
