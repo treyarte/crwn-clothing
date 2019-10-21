e5952be83df54d2f5129e51f7d4ae98c39957ab5
Trey <tajohns22@gmail.com>
2019-10-21 14:14:14 -0500

    implement code that add hoc withspinner to collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,68 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import {connect} from "react-redux";
+import withSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 import {firestore, convertCollectionsSnapshotToMap} from "../../firebase/firebase.utils";
 import {updateCollections} from "../../redux/shop/shop.actions";
 
+const CollectionOverviewWithSpinner = withSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = withSpinner(CollectionPage);
+
 class ShopPage extends React.Component{
+  state ={
+    loading: true
+  }
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const {updateCollections} = this.props;
     const collectionRef = firestore.collection("collection");
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-      
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionsMap);
-      updateCollections(collectionsMap);
-    })
+    //observable pattern 
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   console.log(collectionsMap);
+    //   updateCollections(collectionsMap);
+    //   this.setState({loading: false});
+    // })
+    // promise ex
+    collectionRef.get().then(
+      snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        console.log(collectionsMap);
+        updateCollections(collectionsMap);
+        this.setState({loading: false});
+      });
+//fetch ex
+    // fetch("https://firestore.googleapis.com/v1/projects/crwn-db-1ca08/databases/(default)/documents/collection")
+    // .then(response => response.json())
+    // .then(collection => console.log(collection));
   }
 
   componentWillUnmount(){
-    this.unsubscribeFromSnapshot();
+    // this.unsubscribeFromSnapshot();
   }
 
   render(){
     const {match} = this.props;
+    const {loading} = this.state;
     return (
       <div className="shop-page">
       
-        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        <Route 
+          exact 
+          path={`${match.path}`} 
+          render={props =>(
+            <CollectionOverviewWithSpinner isLoading={loading}{...props}/>
+            )}
+        />
+        <Route 
+          path={`${match.path}/:collectionId`} 
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props}/>
+          )}
+        />
       </div>
     );
   }
